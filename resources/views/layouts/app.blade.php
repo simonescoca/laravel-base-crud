a4ec848b93e73443644c6d2d9eae1f7338ebe240
simonescoca <simone.scoca@icloud.com>
2023-07-27 17:55:09 +0200

    added app layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,17 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    @include("partials.ex1")
-    @include("partials.ex2")
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>@yield("title", "Beaches")</title>
+    </head>
+    <body>
+        @include("partials.header")
 
+        @yield("main")
 
+        @include("partials.footer")
 
-    @vite("resources/js/app.js")
-</body>
+        @vite("resources/js/app.js")
+
+        @yield("custom-script-tail")
+    </body>
 </html>
