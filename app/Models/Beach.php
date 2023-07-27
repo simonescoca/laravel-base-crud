08667fdbdd2dd77160d4692ff79967522a08e47e
simonescoca <simone.scoca@icloud.com>
2023-07-27 17:52:15 +0200

    added model

---
+++ app/Models/Beach.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Beach extends Model
+{
+    use HasFactory;
+}
