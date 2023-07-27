91f53a8c5695a75c96cc7e58f3fa6eac064b8d16
simonescoca <simone.scoca@icloud.com>
2023-07-27 17:55:48 +0200

    added basic header with links

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,21 @@
+<header>
+    <div class="container">
+        <ul>
+            <li>
+                <a href="{{ route("admin.beaches.index") }}">
+                    Admin Home
+                </a>
+            </li>
+            <li>
+                <a href="{{ route("guest.home") }}">
+                    Guest Home
+                </a>
+            </li>
+            <li>
+                <a href="{{ route("admin.beaches.create") }}">
+                    Create New Element
+                </a>
+            </li>
+        </ul>
+    </div>
+</header>
