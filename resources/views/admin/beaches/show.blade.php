11ecf700ccec2d4d633f249176f9146bba0e8d9e
simonescoca <simone.scoca@icloud.com>
2023-07-28 17:35:51 +0200

    fixed path of admin layout

--- resources/views/admin/beaches/show.blade.php
+++ resources/views/admin/beaches/show.blade.php
@@ -1,4 +1,4 @@
-@extends("layouts.app")
+@extends('layouts.admin.app')
 
 @section("title", "Show a beach")
 
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,4 +1,4 @@
-@extends("layouts.app")
+@extends("layouts.guest.app")
 
 @section("title", "Show a beach")
 
