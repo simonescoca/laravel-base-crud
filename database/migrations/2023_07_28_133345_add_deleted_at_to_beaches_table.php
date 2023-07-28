b6595ad82f631e6eac4a3a394ca16c5d85f83e75
GabrieleMajocchi <gabriele.majocchi@gmail.com>
2023-07-28 16:41:25 +0200

    test

--- database/migrations/2023_07_28_133345_add_deleted_at_to_beaches_table.php
+++ database/migrations/2023_07_28_133345_add_deleted_at_to_beaches_table.php
@@ -26,7 +26,7 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::table('beaches', function (Blueprint $table) {
+        Schema::table('beaches',  function (Blueprint $table) {
             //
             $table->dropSoftDeletes();
         });
