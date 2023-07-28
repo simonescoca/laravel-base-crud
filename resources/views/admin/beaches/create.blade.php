21178793a447c7255c769cea0be8db60e3f065bc
simonescoca <simone.scoca@icloud.com>
2023-07-28 11:06:49 +0200

    added message of item deleted

--- resources/views/admin/beaches/create.blade.php
+++ resources/views/admin/beaches/create.blade.php
@@ -6,6 +6,17 @@
 <div class="container">
     <div class="row justify-content-around">
         <div class="col-8">
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+        </div>
+        <div class="col-8">
             <h1>
                 Create a new beach record
             </h1>
@@ -30,46 +41,49 @@
                 <label for="umbrella_number" class="form-label">
                     Umbrella number
                 </label>
-                <input type="text" class="form-control" id="umbrella_number" name="umbrella_number">
+                <input type="number" class="form-control" id="umbrella_number" name="umbrella_number">
             </div>
             <div class="mb-3">
-                <label for="sunbed_price" class="form-label">
+                <label for="sunbed_number" class="form-label">
                     Sunbed number
                 </label>
-                <input type="text" class="form-control" id="sunbed_price" name="sunbed_price">
+                <input type="number" class="form-control" id="sunbed_number" name="sunbed_number">
             </div>
 
             <div class="mb-3">
                 <label for="umbrella_price_per_day" class="form-label">
                     umbrella price per day
                 </label>
-                <input type="date" class="form-control" id="umbrella_price_per_day" name="umbrella_price_per_day">
+                <input type="number" class="form-control" id="umbrella_price_per_day" name="umbrella_price_per_day">
             </div>
             <div class="mb-3">
                 <label for="opening_date" class="form-label">
                     Opening date
                 </label>
-                <input type="text" class="form-control" id="opening_date" name="opening_date">
+                <input type="date" class="form-control" id="opening_date" name="opening_date">
             </div>
             <div class="mb-3">
                 <label for="closing_date" class="form-label">
                     Closing date
                 </label>
-                <input type="text" class="form-control" id="closing_date" name="closing_date">
+                <input type="date" class="form-control" id="closing_date" name="closing_date">
             </div>
+
             <div class="mb-3">
                 <label for="beachvolley_court" class="form-label">
                     Beachvolley court
                 </label>
-                <input type="text" class="form-control" id="beachvolley_court" name="beachvolley_court">
+                <input type="hidden" name="beachvolley_court" id="beachvolley_court" value="0">
+                <input type="checkbox" name="checkbox" id="checkbox" onchange="document.getElementById('beachvolley_court').value = this.checked ? '1' : '0'">
             </div>
-             <div class="mb-3">
+
+            <div class="mb-3">
                 <label for="football_court" class="form-label">
                     Football court
                 </label>
-                <input type="text" class="form-control" id="football_court" name="football_court">
+                <input type="hidden" name="football_court" id="football_court" value="0">
+                <input type="checkbox" name="checkbox" id="checkbox" onchange="document.getElementById('football_court').value = this.checked ? '1' : '0'">
             </div>
-
 
             <button type="submit" class="btn btn-primary">
                 Create new Beach record
