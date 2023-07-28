da25832cd04e5ee766b70be3f948b5345c42734b
simonescoca <simone.scoca@icloud.com>
2023-07-28 16:43:13 +0200

    updated routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\BeachController as AdminController;
+use App\Http\Controllers\Guest\BeachController as GuestController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,12 +15,16 @@ use App\Http\Controllers\Admin\BeachController as AdminController;
 |
 */
 
-Route::get('/', function () { return view('home'); })->name('guest.home');
+Route::get('/', [GuestController::class, 'index'])->name('guest.home');
+
 Route::get('admin/beaches/deleted', [AdminController::class, 'trashed'])->name('admin.beaches.trashed');
 Route::get('admin/beaches', [AdminController::class, 'index'])->name('admin.beaches.index');
 Route::get('admin/beaches/create', [AdminController::class, 'create'])->name('admin.beaches.create');
 Route::post('admin/beaches', [AdminController::class, 'store'])->name('admin.beaches.store');
 Route::get('admin/beaches/{id}', [AdminController::class, 'show'])->name('admin.beaches.show');
+
+Route::get('/{id}', [GuestController::class, 'show'])->name('guest.show');
+
 Route::get('admin/beaches/{id}/edit', [AdminController::class, 'edit'])->name('admin.beaches.edit');
 Route::put('admin/beaches/{id}', [AdminController::class, 'update'])->name('admin.beaches.update');
 Route::delete('admin/beaches/{id}', [AdminController::class, 'destroy'])->name('admin.beaches.destroy');
